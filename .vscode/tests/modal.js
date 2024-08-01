5a375a70405fc03d604bf8c218f5c1d888c5226d
gigigisa <cherryblossomcb24@gmail.com>
2024-08-01 20:13:05 -0300

    agora vai

---
+++ .vscode/tests/modal.js
@@ -0,0 +1,6 @@
+const btnDescontoAbrir = document.querySelector(".btn-desconto")
+const modal = document.querySelector("#modal")
+
+btnDescontoAbrir.onclick = function(){
+modal.showModal()
+}
---
+++ paginas/modal.js
@@ -0,0 +1,3 @@
+const abrirModal = document.querySelector ("#btnAbrirModal");
+const fecharModal = document.querySelector ("#btnAbril");
+const modal = document. querySelector ("#modal");
